470f143763d7eea5fc896d37d3064feee6a96168
DaeWon9 <qkreodnjs97@naver.com>
2025-02-27 22:24:48 +0900

    feat: Router 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Login, Signup } from "./pages";
+
 function App() {
-    return <></>;
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
