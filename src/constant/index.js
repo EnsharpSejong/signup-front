5569899910bc157f79c83cdbb0ec06d97e7ab040
DaeWon9 <qkreodnjs97@naver.com>
2025-02-27 21:00:51 +0900

    feat: constant string 추가

---
+++ src/constant/index.js
@@ -0,0 +1,13 @@
+export const helpTextMap = {
+    name: "공백 기입 불가능",
+    email: "유효하지 않은 메일입니다.",
+    password: "8~16자의 영문 대/소문자, 숫자, 특수문자를 포함해주세요",
+    address: "주소를 입력해주세요",
+};
+
+export const verificationTextMap = {
+    send: "인증번호가 전송되었습니다.",
+    resend: "인증번호가 재전송되었습니다.",
+    emailFail: "올바르지 않은 이메일입니다.",
+    verificationFail: "인증번호가 일치하지 않습니다.",
+};
