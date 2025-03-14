36f779d29865a4e3889e706654195c6d3dd09239
DaeWon9 <qkreodnjs97@naver.com>
2025-03-14 17:14:25 +0900

    fix: change email verification api uri

--- src/apis/verification/index.js
+++ src/apis/verification/index.js
@@ -3,14 +3,14 @@ import { apiPostRequest } from "..";
 export const verificationApi = {
     // 전송
     request: async (email) => {
-        return await apiPostRequest(`${import.meta.env.VITE_API_BASE_URL}/api/v1/email/verification/request`, {
+        return await apiPostRequest(`${import.meta.env.VITE_API_BASE_URL}/api/v1/email-verification/request`, {
             email,
         });
     },
 
     // 확인
     confirm: async ({ email, code }) => {
-        return await apiPostRequest(`${import.meta.env.VITE_API_BASE_URL}/api/v1/email/verification/confirm`, {
+        return await apiPostRequest(`${import.meta.env.VITE_API_BASE_URL}/api/v1/email-verification/confirm`, {
             email,
             code,
         });
