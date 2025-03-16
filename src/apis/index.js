4f2be16c99a40d08e83dbb6624664bee7da472ce
DaeWon9 <qkreodnjs97@naver.com>
2025-03-16 17:26:08 +0900

    feat: apiGetRequest 구현

--- src/apis/index.js
+++ src/apis/index.js
@@ -2,6 +2,13 @@ import axios from "axios";
 import { userApi } from "./user";
 import { verificationApi } from "./verification";
 
+export const apiGetRequest = async (url, body) => {
+    const { data } = await axios.get(url, body, {
+        headers: { "Content-Type": "application/json" },
+    });
+    return data;
+};
+
 export const apiPostRequest = async (url, body) => {
     const { data } = await axios.post(url, body, {
         headers: { "Content-Type": "application/json" },
