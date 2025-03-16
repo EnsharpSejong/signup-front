b953cd73ae5972799e3d15ce254fd378256aeee3
DaeWon9 <qkreodnjs97@naver.com>
2025-03-16 17:35:03 +0900

    fix: ROUTE

--- src/constant/route.js
+++ src/constant/route.js
@@ -1,5 +1,5 @@
 export const ROUTE_LOGIN_PAGE = "/";
-export const ROUTE_SIGNUP_PAGE = "signup";
-export const ROUTE_MY_INFO_PAGE = "my-info";
-export const ROUTE_FIND_PASSWORD_PAGE = "find-password";
-export const ROUTE_RESET_PASSWORD_PAGE = "reset-password";
+export const ROUTE_SIGNUP_PAGE = "/signup";
+export const ROUTE_MY_INFO_PAGE = "/my-info";
+export const ROUTE_FIND_PASSWORD_PAGE = "/find-password";
+export const ROUTE_RESET_PASSWORD_PAGE = "/reset-password";
